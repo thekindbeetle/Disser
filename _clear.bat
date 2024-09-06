4f71158ab3bf4e12522cfc406db2a94e66616ef9
Vladislav Alexeev <thekindbeetle@gmail.com>
2024-09-06 16:17:17 +0300

    Merge pull request #508 from LSinev/feature/Compatibility202408
    Правки по поводу совместимости после обновления TexLive 2024

---
+++ _clear.bat
@@ -0,0 +1,5 @@
+del /s /q *.synctex.gz
+del /s /q *.log
+del /s /q *.aux
+del /s /q *.synctex.gz(busy)
+del /s /q *.tex.bak
---
+++ build.bat
@@ -0,0 +1,4 @@
+pdflatex dissertation.tex
+biber dissertation.tex
+pdflatex dissertation.tex
+pdflatex dissertation.tex
